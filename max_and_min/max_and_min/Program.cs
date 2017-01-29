686c7ebfe38340aae30194a40f20317a2d8b48c1
sabinaakpaeva <sabina_akpaeva@mail.ru>
2017-01-29 20:24:20 +0600

    max

---
+++ max_and_min/max_and_min/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;   // for working with files
+
+namespace max_and_min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\HOME\lab1\text.txt"); // creats stream for reading 
+            string s = sr.ReadLine(); // "9 64 25"
+            string[] arr = s.Split(); // arr[0] = "9", arr[1] = "64", arr[3]="25"
+            int a = int.Parse(arr[0]); // a = 9
+            int b = int.Parse(arr[1]); // b = 64
+            int c = int.Parse(arr[2]); // c = 25
+            int[] numbers = {a, b, c}; // creat array for integers
+            
+            int max = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max) //checks every number in array for maximum
+                {
+                     max = numbers[i];
+                } 
+            }
+
+            int min = 1000000 ;
+            for (int j = 0; j < numbers.Length; j++) //checks every number in array for minimum
+            {
+                if (numbers[j]<min)
+                {
+                    min = numbers[j];
+                }
+            }
+
+            Console.WriteLine("maximum number is " + max); //shows maximum number on console
+            Console.WriteLine("minimum number is " + min); //shows minimum number on console
+            Console.ReadKey();
+            sr.Close();
+        }
+    }
+}
---
+++ max_and_min/max_and_min/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("max_and_min")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("max_and_min")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b27d803a-55ae-4dc5-9c57-a95ec4611a20")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
