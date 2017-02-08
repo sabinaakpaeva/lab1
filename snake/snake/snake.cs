4a348a40e2e347c89e0beb7dabed57ce6ff19505
sabinaakpaeva <sabina_akpaeva@mail.ru>
2017-02-09 00:05:03 +0600

    far

---
+++ snake/snake/snake.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake
+    {
+        public char sign;
+        public ConsoleColor color;
+        public List<Point> body;
+        int cnt;
+
+        public Snake()
+        {
+            sign ='o';
+            color = ConsoleColor.Yellow;
+            body= new List<Point> ();
+            body.Add(new Point (10, 10));
+            cnt = 1;
+        }
+
+        public void Move (int dx, int dy)
+        {
+            if (cnt % 10 == 0)
+                body.Add(new Point(0, 0));
+
+            for(int i =body.Count - 1; i>= 1; i--)
+            {
+                body[i].x= body[i-1].x;
+                 body[i].y= body[i-1].y;
+            }
+            body[0].x +=dx;
+            body[0].y +=dy;
+
+            if (body[0].x > Console.WindowWidth-10)
+                body[0].x= 1;
+
+            if (body[0].x < 1)
+                body[0].x = Console.WindowWidth-10;
+
+            if (body[0].y > Console.WindowHeight-10)
+                body[0].y = 1;
+
+            if (body[0].y < 1)
+                body[0].y = Console.WindowHeight-10;
+
+            cnt++;
+        }
+
+        public void Draw ()
+        {
+            Console.Clear();
+            Console.ForegroundColor = color;
+           
+            foreach (Point p in body)
+            {
+                Console.SetCursorPosition(p.x, p.y);
+                Console.WriteLine(sign);
+            }
+        }
+
+    }
+}
---
+++ snake/snake/wall.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace Snake
+{
+    class wall
+    {
+        public char sign;
+        public ConsoleColor color;
+        List<Point> body;
+
+        public wall(){
+            sign='*';
+            color = ConsoleColor.Green;
+            body = new List<Point>();
+            StreamReader sr = new StreamReader("input.txt");
+            int n = int.Parse(sr.ReadLine());
+            for (int i= 0; i<n; i++) 
+            {
+                string line = sr.ReadLine();
+                for(int j=0; j<line.Length; j++)
+                {
+                    if (line[j]=='*')
+                        body.Add(new Point(j,i));
+                }
+            }
+            sr.Close();
+        }
+
+             public void Draw ()
+        {
+            Console.ForegroundColor = color;
+            foreach (Point p in body)
+            {
+                Console.SetCursorPosition(p.x, p.y);
+                Console.WriteLine(sign);
+
+            }
+             }
+
+        }
+
+    }
+
