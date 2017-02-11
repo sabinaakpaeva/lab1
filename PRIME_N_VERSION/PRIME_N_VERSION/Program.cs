6b8001a10828e084a9dcb531eea36d3c956dcf27
sabinaakpaeva <sabina_akpaeva@mail.ru>
2017-02-11 12:51:10 +0600

    student

--- PRIME_N_VERSION/PRIME_N_VERSION/Program.cs
+++ PRIME_N_VERSION/PRIME_N_VERSION/Program.cs
@@ -10,26 +10,39 @@ namespace PRIME_N_VERSION
     {
         static void Main(string[] args)
         {
-            int count = 0;  // counts number of divided numbers
-            
-            for (int i = 0; i < args.Length; i++) // cycle for entered numbers
-            {
-                count = 0;
-                for (int j = 1; j <= int.Parse(args[i]); j++) // 1 2 3 4 5 ... divides till number in args
-                {
-                    if (int.Parse(args[i]) % j == 0) // checks whether the number is odd or even
-                    {
-                        count++;
-                    }
-                }
+            Console.WriteLine("enter the numbers");
+            string s = Console.ReadLine(); // "7 8 5"
+            string[] arr = s.Split(); // arr[0] = "7", arr[1] = "8", arr[3]="5"
 
-                if(count == 2) // checks whether the number is prime or not
+            int[] numbers = new int[arr.Length];//new array to store numbers as integers
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                numbers[i] = int.Parse(arr[i]); // converting into integer
+            }
+
+            for (int i = 0; i <numbers.Length; i++)
+            {
+               int count = 0;  // counts number of divided numbers
                 {
-                    Console.WriteLine(args[i]);
+                    count = 0;
+                    for (int j = 1; j <= numbers[i]; j++) // 1 2 3 4 5 ... divides till number in args
+                    {
+                        if (numbers[i] % j == 0) 
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (count == 2) // checks whether the number is prime or not
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                    count = 0;
                 }
-                count = 0;
             }
             Console.ReadKey();
         }
     }
+    
 }
--- Student/Student/Program.cs
+++ Student/Student/Program.cs
@@ -14,14 +14,14 @@ namespace Student
             public string lastName;
             public int gpa;
 
-            public Student(string _firstName, string _lastName, int _gpa) //function with parameters of type string and integer
+            public Student(string _firstName, string _lastName, int _gpa) //constructor with parameters of type string and integer
             {
                 firstName = _firstName; // assign parameters to variables
                 lastName = _lastName;
                 gpa = _gpa;
             }
 
-            public override string ToString() //constructor 
+            public override string ToString() //method
             {
                 return firstName + " " + lastName + ", gpa " + gpa;
             }
